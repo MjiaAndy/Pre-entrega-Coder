0501fb7d6bfc86b66f86f474100fdd473ff5cf2c
MjiaAndy <armmcarp123@gmail.com>
2025-05-14 11:56:06 -0300

    Cambios visuales - falta pulir

---
+++ assets/toasts.js
@@ -0,0 +1,12 @@
+const toastTrigger = document.getElementById('liveToastBtn')
+const toastLiveExample = document.getElementById('liveToast')
+
+if (toastTrigger) {
+    const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toastLiveExample)
+    toastTrigger.addEventListener('click', () => {
+        animation: true
+        autohide:true
+        delay: 1500
+    toastBootstrap.show()
+    })
+}
